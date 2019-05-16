32643413a43e1947a4a6064eddfcb7f007f434e9
David Parseen <david.parseen@gmail.com>
2019-05-16 22:15:49 +0300

    Added the background task that runs on a separate thread as a daemon

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-
 # Initialize the app
+
 app = Flask(__name__, instance_relative_config=True)
 
 # Load the config file
@@ -14,6 +14,10 @@ db = SQLAlchemy(app)
 
 #Initialize migrate
 migrate = Migrate(app, db)
+
+# Create a flask instance
+from app.background_db_task import start_thread
+start_thread(db)
 
 # Load the views
 from app import routes
---
+++ app/background_db_task.py
@@ -0,0 +1,46 @@
+# Here lies the task that periodically populate the Quotes table in the database with
+# a random safe for work (mostly programming) joke from https://official-joke-api.appspot.com/jokes/random
+#  Expected data shapes:
+#  Example 1. 
+#     {
+#         "id": 56,
+#         "type": "programming",
+#         "setup": "How do you check if a webpage is HTML5?",
+#         "punchline": "Try it out on Internet Explorer"
+#     }
+# 
+
+import requests
+from threading import Timer
+from app.models import Joke
+import time
+
+
+
+url = "https://official-joke-api.appspot.com/jokes/random"
+    
+
+def fetch_joke(url):
+    return requests.get(url).json()
+
+def write_to_db(db):
+    # Data cannot be passed as a param because it will be prefetched
+    # by the thread and reused.
+    data = fetch_joke(url)
+    if Joke.query.filter_by(setup=data['setup']).first() is None:
+        joke = Joke(_type=data['type'], setup=data['setup'], punchline=data['punchline'])
+        db.session.add(joke)
+        db.session.commit()
+    
+def start_thread(db):
+    class RepeatTimer(Timer):
+        def run(self):
+            while not self.finished.wait(self.interval):
+                self.function(*self.args, **self.kwargs)
+
+    # Call the write to db function every 5 seconds
+    thread = RepeatTimer(5,write_to_db, [db])
+    # Make thread run in the background
+    thread.daemon=True
+    # Start thread
+    thread.start()
